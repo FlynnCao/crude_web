1795d21b215447106e7e85e339626bff9c302981
RainSilver <ppstream123@126.com>
2019-03-24 22:34:58 +0800

    新增发出页的批号或日期搜索功能

--- asserts/js/master.js
+++ asserts/js/master.js
@@ -5,7 +5,7 @@
  * @modify date 2019-03-23 09:04:52
  * @desc [对所有页面适用的交互逻辑]
  */
- 
+
 //刷新当前页面
 function refreshWebPage(){
     location.replace(location);
--- asserts/js/send.js
+++ asserts/js/send.js
@@ -11,7 +11,7 @@
 //refreshBatchInfo();
 /*系统变量区 */
 //存储当前页和所有页
-var allPages = new Array(1, 2, 3 );
+var allPages = new Array(1, 2, 3);
 var currentPage = 0;
 var firstIntoPage = true;
 var cars = new Array(); //存储车辆列表
@@ -19,11 +19,13 @@ var cars_i = 0; //存储车辆列表的下标
 //第一次启动时，载入发车页-今日批次
 if (firstIntoPage) {
     switchPages(0);
-    switchSentBatches(0);
+    switchSentBatches(1);
 
 
 }
- //showAddBatchConsole(false);
+/*默认自动启动的函数 */
+//1.search_pairngString
+//2.handleSearchButton
 //当前或者历史批次
 var currentBatch = 0;
 /*功能函数区 */
@@ -76,42 +78,42 @@ function showAddBatchConsole(flag) {
     }
 }
 //新增批次-显示车辆选择列表
-function showSelectCarList(flag){
-    if(flag == true){
+function showSelectCarList(flag) {
+    if (flag == true) {
         document.querySelector("#addBatchConsole_carMenuBack").style.display = "none";
         document.querySelector("#addBatchConsole_carMenu").style.display = "flex";
-    }else{
+    } else {
         document.querySelector("#addBatchConsole_carMenuBack").style.display = "block";
         document.querySelector("#addBatchConsole_carMenu").style.display = "none";
     }
-  
+
 
 }
 //新增批次-控制车辆点击选择添加
-function addBatch_handleCarListClick(){
+function addBatch_handleCarListClick() {
     let flag = false;
     var rows = document.querySelectorAll("#addBatchConsole>div>div>table>tbody tr");
     for (let index = 0; index < rows.length; index++) {
-        rows[index].onclick = function (){
+        rows[index].onclick = function () {
             this.className = "batch_car_list_checked";
             let carNum = this.children[1].innerHTML;
             //查重
             cars.forEach(element => {
-                if(element == carNum){
+                if (element == carNum) {
                     flag = true;
                 }
             });
-            if(flag == false){
+            if (flag == false) {
                 cars[cars_i++] = carNum;
             }
             flag = false;
             // console.log(this.children[1].innerHTML);
         }
-        
+
     }
 }
 //新增批次-保存选择的车辆列表
-function saveSelectedCarList(){
+function saveSelectedCarList() {
     var carsStr = "";
     // cars.forEach(element => {
     //     console.log(element);
@@ -123,120 +125,35 @@ function saveSelectedCarList(){
     showSelectCarList(false);
     console.log(carsStr);
 }
-// //动画：新增批次完成后，倒序播放动画
-// function playSecondAnimationReverse(){
-//     var block = document.querySelector("#addBatchConsole");
-//     block.style.animation = "hideSecondFloorConsole_addBatch 0.5s";
-// }
+//右侧-搜索按钮绑定事件-历史批
+(function handleSearchButton() {
+    document.querySelector("#col_right_top_search>span").onclick = function () {
+        var info = document.querySelector("#col_right_top_search>input").value;
+        // console.log(info);
+        var historyList = document.querySelector("#col_right_page1_list2");
+        //undefined 空的变量 null 空的对象
+        if (historyList != null) {
+            // console.log("list exist!");
+            // var batchNums = document.querySelectorAll("#col_right_page1_list2>table>tbody tr>td:nth-child(2)");
+            var rows = document.querySelectorAll("#col_right_page1_list2>table>tbody tr");
+            for (let index = 0; index < rows.length; index++) {
+                var num = rows[index].children[1].innerHTML; //获取每一行行对应的发出批号
+                var date = rows[index].children[5].innerHTML; //获取每一行行对应的日期
+                if(search_pairngString(info, num) || search_pairngString(info, date)){
+                   rows[index].style.display = "table-row";
+                }else{
+                    rows[index].style.display = "none";
 
-// function bindingAddBatchConfirm() {
-//     //绑定新增批次确认按钮
-//     document.querySelector("#addBatch_ok").onclick = function () {
-//         //       addBatchSubmit();
-
-//        // batchUpdateJSON();
-//      //   refreshBatchInfo();
-
-//     }
-//     document.querySelector("#addBatch_refresh").onclick = function () {
-//         //       addBatchSubmit();
-
-//        // batchUpdateJSON();
-//     }
-// }
-
-// //新增批次-确认操作
-// function batchUpdateJSON() {
-//     // var num = document.querySelector("#addBatchConsole>form input");
-//     // console.log(num.length);
-
-//     //1批号 2日期 3油量 4水分 5密度 6车数
-//     var carNum = document.getElementById("abc_num").value;
-//     console.log(carNum);
-
-//     // var oil = document.querySelector("#addBatchConsole>form input:nth-child(3)").value;
-//     // var water = document.querySelector("#addBatchConsole>form input:nth-child(4)").value;
-//     // var density = document.querySelector("#addBatchConsole>form input:nth-child(5)").value;
-
-//     $.ajaxSettings.async = true;
-
-//     $.getJSON("data/currentBatch.json",
-//         function (data) {
-//             //传入JSON数据
-//             console.log("导入发出批次信息");
-//             $.each(data, function (key, value) {
-//                switch (key) {
-//                    case "batch_density":
-//                     value = "2.3232"
-//                     console.log(value);
-//                        break;
-               
-//                    default:
-//                        break;
-//                }
-//                 //遍历JSON对象
-                
-                
-//                 // switch (key) {
-//                 //     case "batch_car_num":
-//                 //         value = carNum;
-//                 //         break;
-//                 //     case "batch_oil":
-//                 //         // value = oil;
-//                 //         break;
-//                 //     case "batch_water":
-//                 //         // value = water;
-//                 //         break;
-//                 //     case "batch_density":
-//                 //         // value = density;
-//                 //         break;
-//                 //     default:
-//                 //         break;
-//                 // }
-
-//             });
-//         }   
-//     );
-//     // refreshBatchInfo();
-//     $.ajaxSettings.async = false;
-
-
-// }
-// //刷新列表的状态和批次信息
-// function refreshBatchInfo() {
-
-//     $.ajaxSettings.async = true;
-
-//         $.getJSON("data/currentBatch.json",
-//         function (data) {
-//             //传入JSON数据
-//             console.log("刷新发出批信息");
-//             $.each(data, function (key, value) {
-//                 //遍历JSON对象
-//                 switch (key) {
-//                     case "batch_car_num":
-//                         console.log(value);
-//                         break;
-//                     case "batch_oil":
-//                         console.log(value);
-//                         break;
-//                     case "batch_water":
-//                         console.log(value);
-//                         break;
-//                     case "batch_density":
-//                         console.log(value);
-//                         break;
-//                     default:
-//                         break;
-//                 }
-
-//             });
-//         }
-
-
-       
-
-//     );
-//     $.ajaxSettings.async = false;
-
-// }
+                }
+            }
+        }
+    }
+})();
+//搜索-字符串匹配
+function search_pairngString(obj, str) {
+    if (str.indexOf(obj) >= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
